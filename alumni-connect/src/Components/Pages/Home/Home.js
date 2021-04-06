4555d42c20bc54af429bcd4e4620afc70b72f79e
Shambhavi Sharma <60618802+ShambhaviSharma0110@users.noreply.github.com>
2021-04-06 20:40:06 +0530

    Update Home.js

--- alumni-connect/src/Components/Pages/Home/Home.js
+++ alumni-connect/src/Components/Pages/Home/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <>
             <h1>Hello!</h1>
-            <h2>Welcome to Hire-smart!</h2>
+            <h2>Welcome to Alumni-connect!</h2>
         </>
     )
 }
