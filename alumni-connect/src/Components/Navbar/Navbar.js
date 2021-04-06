e5db59ee47ecb83e6144d541062f5d091b9537e1
Shambhavi Sharma <60618802+ShambhaviSharma0110@users.noreply.github.com>
2021-04-06 20:38:23 +0530

    Update Navbar.js

--- alumni-connect/src/Components/Navbar/Navbar.js
+++ alumni-connect/src/Components/Navbar/Navbar.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import './Navbar.css';
-import {Link, BrowserRouter as Router} from 'react-router-dom';
-
-
+import React from "react";
+import "./Nav.css";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-      <div className = "nav">
-        <Router>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About-us</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact-Us</Link>
-            </li>
-            <li>
-              <Link to="/">Services</Link>
-            </li>
-          </ul>
-        </Router>
-      </div>
-    );
-}
+  return (
+    <div className="nav">
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About-us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact-Us</Link>
+          </li>
+          <li>
+            <Link to="/">Services</Link>
+          </li>
+        </ul>
+      </Router>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
