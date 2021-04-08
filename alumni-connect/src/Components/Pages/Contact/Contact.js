7c7af59bce47f6cd308d13c7f9a8951db562ff30
Shambhavi Sharma <60618802+ShambhaviSharma0110@users.noreply.github.com>
2021-04-08 20:13:39 +0530

    added content

--- alumni-connect/src/Components/Pages/Contact/Contact.js
+++ alumni-connect/src/Components/Pages/Contact/Contact.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import img3 from "../../../assets/img3.png";
 
 const Contact = () => {
+    const myStyle = {
+      width: "50%",
+      height: "50%",
+    };
+
+    const imgStyle = {
+      position: "absolute",
+      top: "50%",
+      left: "70%",
+      transform: "translate(-50%, -50%)",
+    };
+
     return (
-        <div>
-            <h1>Contact us</h1>
+      <div>
+        <img src={img3} id="img2" alt="cap" style={myStyle} />
+        <div className="text1">
+          <p style={imgStyle}>
+            <h1>Contact-us</h1>
+            <br />
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat nam debitis excepturi, ut corrupti totam exercitationem veniam ullam dolores illo minima, vero consequuntur molestiae nisi odio eveniet rem quidem doloremque.
+          </p>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Contact
