220256aeff28b036caa4d01994e8433ec379e6b8
Shambhavi Sharma <60618802+ShambhaviSharma0110@users.noreply.github.com>
2021-04-08 19:54:43 +0530

    content added

--- alumni-connect/src/Components/Pages/Home/Home.js
+++ alumni-connect/src/Components/Pages/Home/Home.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import img from '../../../assets/img.jpg';
 import './Home.css';
 
 const Home = () => {
     return (
-        <>
-            <h1>Hello!</h1>
-            <h2>Welcome to Alumni-connect!</h2>
-        </>
-    )
+      <div>
+        <img src={img} id= "img1" />
+        <div className="text">
+          <h2>Welcome to Alumni Connect!</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex
+            dolorum, aliquid quaerat omnis quia mollitia odio perspiciatis saepe
+            praesentium alias molestiae ad explicabo suscipit culpa, illo et
+            dicta, neque error?
+          </p>
+        </div>
+      </div>
+    );
 }
 
 export default Home
