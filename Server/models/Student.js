3becf65948ad054d7602c578b82af20f676574ce
mitalirs <mitalirs1701@gmail.com>
2021-04-21 13:21:48 +0530

    Create Student.js
    created a 'models' folder inside server directory and inside that created a student.js file

---
+++ Server/models/Student.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        unique: true, 
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)) {
+                throw new Error('Invalid Email')
+            }
+        }
+    },
+    contact:{
+        type:String,
+        required:true
+    },
+    branch:{
+        type:String,
+        required: true,
+        trim: true,
+    },
+    year:{
+        type:String,
+        required: true,
+        trim: true,
+    },
+    degree:{
+        type:String,
+        required: true,
+        trim: true,
+    },
+    institutionName:{
+        type:String,
+        required: true,
+        trim: true,
+    },
+    password:{
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value){
+            if(value.toLowerCase().includes("password")){
+                throw new Error ("cannot contain password")
+            }
+        }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }],
+}, { 
+    timestamps: true 
+})
+
+
+const Student = mongoose.model('Student', studentSchema) 
+module.exports = Student
