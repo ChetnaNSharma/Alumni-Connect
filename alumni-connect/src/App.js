377a1ba777313a8b432f808fba85d2f70f9c5385
Chitvan Ramani <chitvanramani@gmail.com>
2021-04-21 22:41:55 +0530

    scroll-button

--- alumni-connect/src/App.js
+++ alumni-connect/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
+import Scroll from './Components/ScrollToTop/ScrollToTop'
 import Home from './Components/Pages/Home/Home';
 import Contact from './Components/Pages/Contact/Contact';
 import About from './Components/Pages/About/About';
 import Error from './Components/Pages/Error/Error';
 
- import Service from './Components/Pages/Service/Service';
+import Service from './Components/Pages/Service/Service';
 import Footer from './Components/Pages/Footer/Footer'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
@@ -16,21 +17,16 @@ function App() {
       <Navbar />
       <Router>
         <Switch>
-
- 
-       
-    
-
-
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
-    <Route exact path="/service" component={Service} />
+          <Route exact path="/service" component={Service} />
           <Route component={Error} />
 
         </Switch>
       </Router>
       <Footer/>
+      <Scroll />
     </div>
   );
 }
