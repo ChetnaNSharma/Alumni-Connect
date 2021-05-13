b4f529f4499365b89ff26aa8d0934e293f45d0cf
Chitvan Ramani <chitvanramani@gmail.com>
2021-05-13 12:48:20 +0530

    splash-screen

---
+++ alumni-connect/src/Components/Preloader/Preloader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Preloader.css";
+
+function Preloader() {
+  return (
+    <div className="preloader">
+      <div className="updown">
+        <h1>
+          ALUMNI-CONNECT<span> .</span>
+          <span> .</span>
+          <span> .</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Preloader;
